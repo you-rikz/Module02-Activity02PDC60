5d089c32994dda9d6d40d76b6ed8cb2f25faf617
you-rikz <arnoldlim0337@gmail.com>
2023-09-22 11:06:09 +0800

    mod2-act3

---
+++ PDC60Module02/PDC60Module02/ViewModel/NumericValidationBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace PDC60Module02.ViewModel
+{
+    public class NumericValidationBehavior:Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            double result;
+            bool isValid = double.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Green : Color.Red; 
+        }
+
+    }
+}
